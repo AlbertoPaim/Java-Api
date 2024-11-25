cd83601cb6f1bcb166e4adecd7610d842af6e817
Alberto Paim <albertoopaim@gmail.com>
2024-11-25 09:17:30 -0300

    FindProductById

--- src/main/java/br/com/albertopaim/apirest/controllers/ProductController.java
+++ src/main/java/br/com/albertopaim/apirest/controllers/ProductController.java
@@ -3,11 +3,15 @@ package br.com.albertopaim.apirest.controllers;
 import br.com.albertopaim.apirest.models.Product;
 import br.com.albertopaim.apirest.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -18,6 +22,17 @@ public class ProductController {
     @GetMapping()
     public List<Product> getProduct() {
         return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> obterProduto(@PathVariable Integer id) {
+        Optional<Product> produto = productRepository.findById(id);
+
+        if(!(produto.isPresent())){
+            return ResponseEntity.notFound().build();
+        }
+        return new ResponseEntity<Product>(produto.get(), HttpStatus.OK);
+
     }
 
 }
